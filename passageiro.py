730c6f1fdc24279a28cab9e65c799c061a05e08e
Murilo Ribeiro <88835239+MuriloRibg@users.noreply.github.com>
2022-09-06 00:22:12 -0300

    Adiciona validação dos campos CPF e RG

--- passageiro.py
+++ passageiro.py
@@ -1,12 +1,24 @@
+import string
+
 class Passageiro:
-    def __init__(self, nome, sobrenome, RG, CPF, endereco, celular, fixo, status, assento):
+    def __init__(self, nome, sobrenome, Rg, Cpf, endereco, celular, fixo, status, assento):
         self.nome = nome
         self.sobrenome = sobrenome
-        self.RG = RG
-        self.CPF = CPF
+        self.SetRg(Rg)
+        self.SetCpf(Cpf)
         self.endereco = endereco
         self.celular = celular
         self.fixo = fixo
         self.status = status
-        self.assento =  assento
+        self.assento = assento
 
+    def SetRg(self, Rg: string) -> None:
+        if (len(Rg) > 8 and len(Rg) < 6):
+            return print('Erro ao inserir RG. Tamanho inválido!')
+        self.Rg = Rg
+
+    def SetCpf(self, Cpf: string) -> None:
+        if (len(Cpf) != 14):
+            return print('Erro ao inserir CPF. Tamanho inválido!')
+        self.Cpf = Cpf
+    
